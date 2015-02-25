008104bf15887525dafcf5af2755352187043e1c
nickooms <oomsni@gmail.com>
2015-02-25 10:41:36 +0100

    byebye xxx

--- SplitTiles.js
+++ SplitTiles.js
@@ -185,7 +185,7 @@ $S = window.SplitTiles = {
 						$S.objectIndex++;
 						$S.joinObject();
 					} else {
-						alert('Done xxxxxxxxx [' + (new Date().getTime() - $S.start) + ' ms]');
+						alert('Done [' + (new Date().getTime() - $S.start) + ' ms]');
 					}
 				});
 			}
